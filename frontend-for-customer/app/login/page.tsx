136c5ac50014493bba05a80156a211de5787ae76
ksw-1024 <orimun.mohumohu@gmail.com>
2024-04-21 11:10:25 +0900

    Removed login function by email address.

--- frontend-for-customer/app/login/page.tsx
+++ frontend-for-customer/app/login/page.tsx
@@ -32,19 +32,6 @@ export default function Login() {
                             アカウントにログインしてください。
                         </CardDescription>
                     </CardHeader>
-                    <CardContent className="grid gap-4">
-                        <div className="grid gap-2">
-                            <Label htmlFor="email">Email</Label>
-                            <Input id="email" type="email" placeholder="m@example.com" required />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label htmlFor="password">Password</Label>
-                            <Input id="password" type="password" required />
-                        </div>
-                    </CardContent>
-                    <CardContent>
-                        <Button className="w-full">Sign in</Button>
-                    </CardContent>
                     <form
                         action={async () => {
                             "use server"
